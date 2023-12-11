d48c25c106b625e1273472eb5210124cd2d5f64a
SannaAb <sannaabrahamsson@gmail.com>
2023-12-11 14:15:43 +0100

    u

---
+++ Scripts/runParser_nextflow.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#$ -S /bin/bash
+#$ -N Parser
+#$ -j y
+#$ -cwd
+#$ -pe mpi 1
+#$ -q development.q
+
+
+module load miniconda/4.14.0
+source activate TaxProfiler
+
+set -x
+
+TaxprofilerOutputFolder=$1
+ParsingOutputFolder=$2
+
+source /medstore/Development/Metagenomics/TaxProfiler/TestSamples_FromMicro/TaxprofilerRun/Scripts/Taxprofiler-Metagenomics/configs/Bash_Config_Taxprofiler.config
+
+nextflow run /medstore/Development/Metagenomics/TaxProfiler/TestSamples_FromMicro/TaxprofilerRun/Scripts/Taxprofiler-Metagenomics/Scripts/ParseTaxprofiler.nf --Taxprofiler_out ${TaxprofilerOutputFolder} --OutputDir ${ParsingOutputFolder} --TaxDump_gz ${taxdumpgz} --DepthTresh ${DepthTresh} -c /medstore/Development/Metagenomics/TaxProfiler/TestSamples_FromMicro/TaxprofilerRun/Scripts/Taxprofiler-Metagenomics/configs/Mandalore_Taxprofiler.config
+
